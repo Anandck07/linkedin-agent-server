ae1ea0ffb31de1a366d565cd0938705779aa5996
Anandck07 <kumbaranand777@gmail.com>
2026-05-14 21:28:21 +0530

    fix: add SMTP timeout to prevent hanging requests

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,11 +1,16 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 export const sendMail = ({ to, subject, html }) =>
